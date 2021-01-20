4cfb5e3efbde04383990a97af775862b02d96800
Guilherme Afonso <gasp@cin.ufpe.br>
2021-01-20 02:29:15 -0300

    Add worker utils to main service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,6 +2,7 @@ self.addEventListener('install', function(event) {
     console.log('Service worker successfully installed.')
 });
 
+importScripts("utils/workerUtils.js");
 importScripts('background/onInstalled.js');
 importScripts('background/onMessage.js');
 importScripts('background/onMessageExternal.js');
