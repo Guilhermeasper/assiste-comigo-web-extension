3eb335ae9a6938b4b3647dea36b816cf21e92d82
Guilherme Afonso <gasp@cin.ufpe.br>
2021-01-20 02:29:36 -0300

    Remove declarative content rules

--- background/onInstalled.js
+++ background/onInstalled.js
@@ -1,4 +1,3 @@
-importScripts("./utils/workerUtils.js");
 importScripts('./libs/socket.io.js');
 
 const serverAddress = "https://assistecomigo.herokuapp.com";
@@ -11,31 +10,13 @@ chrome.runtime.onInstalled.addListener(onInstalled);
  */
 function onInstalled(details) {
     clearInfo();
-    let tmpSocket = io.connect(serverAddress, {
+    let temporarySocket = io.connect(serverAddress, {
         transports: ["websocket"],
     });
-    tmpSocket.on("newId", (data) => {
+    temporarySocket.on("newId", (data) => {
         console.log(`The user created by server is ${data.newId}`);
         setUserId(data.newId);
-        tmpSocket.disconnect();
+        temporarySocket.disconnect();
     });
-    tmpSocket.emit("getId", {});
-
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        console.log("Rules Removed");
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: {
-                            urlMatches:
-                                "(vimeo|crunchyroll|primevideo|viki|youtube|anitube|netflix).(com|site)",
-                        },
-                    }),
-                ],
-                actions: [new chrome.declarativeContent.ShowPageAction()],
-            },
-        ]);
-        console.log("New rules added");
-    });
+    temporarySocket.emit("getId", {});
 }
